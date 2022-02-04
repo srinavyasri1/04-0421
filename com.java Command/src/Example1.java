6583e591aa697fb18861fb8ed85fc55bd04f3f9d
Sreenavya <Sreenavya@192.168.0.102>
2022-02-04 17:05:43 +0530

    example 1 pushed into repository

---
+++ com.java Command/src/Example1.java
@@ -0,0 +1,21 @@
+
+public class Example1
+{
+
+
+public static void main(String[] args)
+	{
+		int CustomerId=1112;
+	    String Customername="sreenavya";
+	    String CustomerAddress="Hyderabad";
+	    System.out.println(CustomerId);
+	    System.out.println(Customername);
+	    System.out.println( CustomerAddress);
+
+	    
+	}
+ 
+}
+	    
+	    		
+	
